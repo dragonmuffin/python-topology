0aad504b0a71ac72507cf438fa904baa13a0b43f
Rostislav <bolsunros@mail.ru>
2026-04-21 22:43:17 +0300

    Added diversity.py Created Interfaces for diversity namespace
    Signed-off-by: Rostislav <bolsunros@mail.ru>

---
+++ diversity.py
@@ -0,0 +1,26 @@
+class Manifold:
+    def euler_characteristic(self):
+        pass
+
+    def homeomorphism(self, a, b):
+        pass
+
+
+class SphereWithHandles(Manifold):
+    def __init__(self):
+        pass
+
+    def homeomorphism(self, a, b):
+        pass
+
+
+class Hieroglyphs(Manifold):
+    def __init__(self):
+        pass
+
+    def homeomorphism(self, a, b):
+        pass
+
+    def get_boundary_circles(self):
+        pass
+
